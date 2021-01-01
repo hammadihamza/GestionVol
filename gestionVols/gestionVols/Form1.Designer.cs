2b09266ac2fa72ae14aa346f64c275031adc1665
hammadihamza <63562586+hammadihamza@users.noreply.github.com>
2021-01-01 20:42:05 +0100

    Add files via upload

---
+++ gestionVols/gestionVols/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace gestionVols
+{
+    partial class GestionVol
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GestionVol));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Numvoltxt = new System.Windows.Forms.TextBox();
+            this.hdeptext = new System.Windows.Forms.TextBox();
+            this.mindeptxt = new System.Windows.Forms.TextBox();
+            this.hartxt = new System.Windows.Forms.TextBox();
+            this.minartxt = new System.Windows.Forms.TextBox();
+            this.cbxvildep = new System.Windows.Forms.ComboBox();
+            this.cbxvilar = new System.Windows.Forms.ComboBox();
+            this.dgvVol = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sauvbtn = new System.Windows.Forms.Button();
+            this.supbtn = new System.Windows.Forms.Button();
+            this.modbtn = new System.Windows.Forms.Button();
+            this.ajbtn = new System.Windows.Forms.Button();
+            this.nvbtn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.nextbtn = new System.Windows.Forms.Button();
+            this.prevbtn = new System.Windows.Forms.Button();
+            this.lastbtn = new System.Windows.Forms.Button();
+            this.firstbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVol)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero vol:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ville départ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ville Arrivée";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Heure Départ:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(282, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Minute Départ:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(282, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Heure arrivée:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(282, 136);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Minute arrivée:";
+            // 
+            // Numvoltxt
+            // 
+            this.Numvoltxt.Location = new System.Drawing.Point(101, 29);
+            this.Numvoltxt.Name = "Numvoltxt";
+            this.Numvoltxt.Size = new System.Drawing.Size(121, 20);
+            this.Numvoltxt.TabIndex = 7;
+            // 
+            // hdeptext
+            // 
+            this.hdeptext.Location = new System.Drawing.Point(376, 26);
+            this.hdeptext.Name = "hdeptext";
+            this.hdeptext.Size = new System.Drawing.Size(100, 20);
+            this.hdeptext.TabIndex = 10;
+            // 
+            // mindeptxt
+            // 
+            this.mindeptxt.Location = new System.Drawing.Point(376, 62);
+            this.mindeptxt.Name = "mindeptxt";
+            this.mindeptxt.Size = new System.Drawing.Size(100, 20);
+            this.mindeptxt.TabIndex = 11;
+            // 
+            // hartxt
+            // 
+            this.hartxt.Location = new System.Drawing.Point(376, 97);
+            this.hartxt.Name = "hartxt";
+            this.hartxt.Size = new System.Drawing.Size(100, 20);
+            this.hartxt.TabIndex = 12;
+            // 
+            // minartxt
+            // 
+            this.minartxt.Location = new System.Drawing.Point(376, 133);
+            this.minartxt.Name = "minartxt";
+            this.minartxt.Size = new System.Drawing.Size(100, 20);
+            this.minartxt.TabIndex = 13;
+            // 
+            // cbxvildep
+            // 
+            this.cbxvildep.FormattingEnabled = true;
+            this.cbxvildep.Location = new System.Drawing.Point(101, 62);
+            this.cbxvildep.Name = "cbxvildep";
+            this.cbxvildep.Size = new System.Drawing.Size(121, 21);
+            this.cbxvildep.TabIndex = 14;
+            // 
+            // cbxvilar
+            // 
+            this.cbxvilar.FormattingEnabled = true;
+            this.cbxvilar.Location = new System.Drawing.Point(101, 96);
+            this.cbxvilar.Name = "cbxvilar";
+            this.cbxvilar.Size = new System.Drawing.Size(121, 21);
+            this.cbxvilar.TabIndex = 15;
+            // 
+            // dgvVol
+            // 
+            this.dgvVol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVol.Location = new System.Drawing.Point(26, 236);
+            this.dgvVol.Name = "dgvVol";
+            this.dgvVol.Size = new System.Drawing.Size(488, 150);
+            this.dgvVol.TabIndex = 16;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sauvbtn);
+            this.groupBox1.Controls.Add(this.supbtn);
+            this.groupBox1.Controls.Add(this.modbtn);
+            this.groupBox1.Controls.Add(this.ajbtn);
+            this.groupBox1.Controls.Add(this.nvbtn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbxvilar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbxvildep);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.minartxt);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.hartxt);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.mindeptxt);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.hdeptext);
+            this.groupBox1.Controls.Add(this.Numvoltxt);
+            this.groupBox1.Location = new System.Drawing.Point(26, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(696, 191);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mise à jour";
+            // 
+            // sauvbtn
+            // 
+            this.sauvbtn.Location = new System.Drawing.Point(571, 140);
+            this.sauvbtn.Name = "sauvbtn";
+            this.sauvbtn.Size = new System.Drawing.Size(75, 23);
+            this.sauvbtn.TabIndex = 24;
+            this.sauvbtn.Text = "Sauvegarder";
+            this.sauvbtn.UseVisualStyleBackColor = true;
+            this.sauvbtn.Click += new System.EventHandler(this.sauvbtn_Click);
+            // 
+            // supbtn
+            // 
+            this.supbtn.Location = new System.Drawing.Point(571, 111);
+            this.supbtn.Name = "supbtn";
+            this.supbtn.Size = new System.Drawing.Size(75, 23);
+            this.supbtn.TabIndex = 23;
+            this.supbtn.Text = "Supprimer";
+            this.supbtn.UseVisualStyleBackColor = true;
+            this.supbtn.Click += new System.EventHandler(this.supbtn_Click);
+            // 
+            // modbtn
+            // 
+            this.modbtn.Location = new System.Drawing.Point(571, 82);
+            this.modbtn.Name = "modbtn";
+            this.modbtn.Size = new System.Drawing.Size(75, 23);
+            this.modbtn.TabIndex = 22;
+            this.modbtn.Text = "Modifier";
+            this.modbtn.UseVisualStyleBackColor = true;
+            this.modbtn.Click += new System.EventHandler(this.modbtn_Click);
+            // 
+            // ajbtn
+            // 
+            this.ajbtn.Location = new System.Drawing.Point(571, 53);
+            this.ajbtn.Name = "ajbtn";
+            this.ajbtn.Size = new System.Drawing.Size(75, 23);
+            this.ajbtn.TabIndex = 21;
+            this.ajbtn.Text = "Ajouter";
+            this.ajbtn.UseVisualStyleBackColor = true;
+            this.ajbtn.Click += new System.EventHandler(this.ajbtn_Click);
+            // 
+            // nvbtn
+            // 
+            this.nvbtn.Location = new System.Drawing.Point(571, 24);
+            this.nvbtn.Name = "nvbtn";
+            this.nvbtn.Size = new System.Drawing.Size(75, 23);
+            this.nvbtn.TabIndex = 20;
+            this.nvbtn.Text = "Nouveau";
+            this.nvbtn.UseVisualStyleBackColor = true;
+            this.nvbtn.Click += new System.EventHandler(this.nvbtn_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.nextbtn);
+            this.groupBox3.Controls.Add(this.prevbtn);
+            this.groupBox3.Controls.Add(this.lastbtn);
+            this.groupBox3.Controls.Add(this.firstbtn);
+            this.groupBox3.Location = new System.Drawing.Point(522, 236);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 150);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Navigation";
+            // 
+            // nextbtn
+            // 
+            this.nextbtn.Location = new System.Drawing.Point(116, 88);
+            this.nextbtn.Name = "nextbtn";
+            this.nextbtn.Size = new System.Drawing.Size(57, 23);
+            this.nextbtn.TabIndex = 28;
+            this.nextbtn.Text = ">";
+            this.nextbtn.UseVisualStyleBackColor = true;
+            this.nextbtn.Click += new System.EventHandler(this.nextbtn_Click);
+            // 
+            // prevbtn
+            // 
+            this.prevbtn.Location = new System.Drawing.Point(17, 88);
+            this.prevbtn.Name = "prevbtn";
+            this.prevbtn.Size = new System.Drawing.Size(57, 23);
+            this.prevbtn.TabIndex = 27;
+            this.prevbtn.Text = "<";
+            this.prevbtn.UseVisualStyleBackColor = true;
+            this.prevbtn.Click += new System.EventHandler(this.prevbtn_Click);
+            // 
+            // lastbtn
+            // 
+            this.lastbtn.Location = new System.Drawing.Point(116, 41);
+            this.lastbtn.Name = "lastbtn";
+            this.lastbtn.Size = new System.Drawing.Size(57, 23);
+            this.lastbtn.TabIndex = 26;
+            this.lastbtn.Text = ">>";
+            this.lastbtn.UseVisualStyleBackColor = true;
+            this.lastbtn.Click += new System.EventHandler(this.lastbtn_Click);
+            // 
+            // firstbtn
+            // 
+            this.firstbtn.Location = new System.Drawing.Point(17, 41);
+            this.firstbtn.Name = "firstbtn";
+            this.firstbtn.Size = new System.Drawing.Size(57, 23);
+            this.firstbtn.TabIndex = 25;
+            this.firstbtn.Text = "<<";
+            this.firstbtn.UseVisualStyleBackColor = true;
+            this.firstbtn.Click += new System.EventHandler(this.firstbtn_Click);
+            // 
+            // GestionVol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 407);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvVol);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GestionVol";
+            this.Text = "Gestion de vols";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVol)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Numvoltxt;
+        private System.Windows.Forms.TextBox hdeptext;
+        private System.Windows.Forms.TextBox mindeptxt;
+        private System.Windows.Forms.TextBox hartxt;
+        private System.Windows.Forms.TextBox minartxt;
+        private System.Windows.Forms.ComboBox cbxvildep;
+        private System.Windows.Forms.ComboBox cbxvilar;
+        private System.Windows.Forms.DataGridView dgvVol;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button sauvbtn;
+        private System.Windows.Forms.Button supbtn;
+        private System.Windows.Forms.Button modbtn;
+        private System.Windows.Forms.Button ajbtn;
+        private System.Windows.Forms.Button nvbtn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button nextbtn;
+        private System.Windows.Forms.Button prevbtn;
+        private System.Windows.Forms.Button lastbtn;
+        private System.Windows.Forms.Button firstbtn;
+    }
+}
+
---
+++ gestionVols/gestionVols/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace gestionVols
+{
+    public partial class GestionVol : Form
+    {
+        public GestionVol()
+        {
+            InitializeComponent();
+        }
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        BindingSource binsou = new BindingSource();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("Select distinct * from vol;",new SqlConnection(ConfigurationManager.ConnectionStrings["ch1"].ConnectionString));
+            da.Fill(ds, "vol");
+            cbxvildep.DisplayMember = "vilar";
+            cbxvildep.ValueMember = "novol";
+            cbxvildep.DataSource = ds.Tables[0];
+            cbxvilar.DisplayMember = "vildep";
+            cbxvilar.DataSource = ds.Tables[0];
+            cbxvilar.ValueMember = "novol";
+            binsou.DataSource = ds.Tables[0];
+            Numvoltxt.DataBindings.Add("text", binsou, "novol");
+            cbxvildep.DataBindings.Add("SelectedItem", binsou, "vildep");
+            cbxvilar.DataBindings.Add("SelectedItem", binsou, "vilar");
+            hdeptext.DataBindings.Add("text", binsou, "dep_h");
+            mindeptxt.DataBindings.Add("text", binsou, "dep_mn");
+            hartxt.DataBindings.Add("text", binsou, "ar_h");
+            minartxt.DataBindings.Add("text", binsou, "ar_mn");
+            dgvVol.DataSource = binsou;
+
+        }
+
+        private void nvbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                binsou.AddNew();
+                
+            }
+            catch(Exception exc)
+            { MessageBox.Show(exc.Message); }
+         }
+
+        private void ajbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                binsou.EndEdit();
+               
+            }
+            catch (Exception exc)
+            { MessageBox.Show(exc.Message); }
+        }
+
+        private void modbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                binsou.EndEdit();
+               
+            }
+            catch (Exception exc)
+            { MessageBox.Show(exc.Message); }
+        }
+
+        private void supbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                binsou.RemoveCurrent();
+                
+            }
+            catch (Exception exc)
+            { MessageBox.Show(exc.Message); }
+
+         }
+
+        private void firstbtn_Click(object sender, EventArgs e)
+        {
+            binsou.MoveFirst();
+        }
+
+        private void lastbtn_Click(object sender, EventArgs e)
+        {
+            binsou.MoveLast();
+        }
+
+        private void prevbtn_Click(object sender, EventArgs e)
+        {
+            binsou.MovePrevious();
+        }
+
+        private void nextbtn_Click(object sender, EventArgs e)
+        {
+            binsou.MoveNext();
+        }
+
+        private void sauvbtn_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                SqlCommandBuilder cmb = new SqlCommandBuilder(da);
+                da.SelectCommand.CommandText = "Select * from vol;";
+                da.Update(ds.Tables[0]);
+                
+            }
+            catch (Exception exc)
+            { MessageBox.Show(exc.Message); }
+        }
+    }
+}
